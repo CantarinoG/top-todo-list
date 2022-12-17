7a94e67bea7efb80217372ee29be93f9ed2b2c8d
CantarinoG <guilhermecantarino1709@gmail.com>
2022-12-17 18:38:22 -0300

    Change nav design

--- src/UI/nav.js
+++ src/UI/nav.js
@@ -1,16 +1,21 @@
+import all from '../icons/all.svg';
+import project from '../icons/project.svg';
+import today from '../icons/today.svg';
+import week from '../icons/week.svg';
+
 export function renderNav(query) {
     query.innerHTML = `
     <ul>
-        <li><button>All</button></li>
-        <li><button>Today</button></li>
-        <li><button>This Week</button></li>
+        <li><img class="icon" src="${all}" alt="All Icon"><button>All</button></li>
+        <li><img class="icon" src="${today}" alt="Today Icon"><button>Today</button></li>
+        <li><img class="icon" src="${week}" alt="This week Icon"><button>This Week</button></li>
     </ul>
     <span>Projects</span>
     <ul>
-        <li><button>Default</button></li>
-        <li><button>Default2</button></li>
-        <li><button>Default3</button></li>
-        <li><button id="add">+ ADD PROJECT</button></li>
+        <li><img class="icon" src="${project}" alt="Project Icon"><button>Default</button></li>
+        <li><img class="icon" src="${project}" alt="Project Icon"><button>Default2</button></li>
+        <li><img class="icon" src="${project}" alt="Project Icon"><button>Default3</button></li>
+        <li id="add-container"><button id="add">Add Project</button></li>
     </ul>
     `;
 }
