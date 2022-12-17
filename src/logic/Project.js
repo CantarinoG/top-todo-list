a1ab7ea95371d7d82d850fdb0fd3a47aad1f5d13
CantarinoG <guilhermecantarino1709@gmail.com>
2022-12-17 19:05:28 -0300

    Add base classes

---
+++ src/logic/Project.js
@@ -0,0 +1,18 @@
+class Project {
+
+
+    constructor(name, tasks) {
+        this.name = name;
+        this.tasks = tasks;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+    getTasks() {
+        return this.tasks;
+    }
+}
---
+++ src/logic/Task.js
@@ -0,0 +1,38 @@
+class Task {
+
+
+    constructor(name, description, date, isCompleted) {
+        this.name = name;
+        this.description = description;
+        this.date = date;
+        this.isCompleted = isCompleted;
+    }
+
+
+    setName(name) {
+        this.name = name;
+    }
+    setDescription(description) {
+        this.description = description;
+    }
+    setDate(date) {
+        this.date = date;
+    }
+    setIsCompleted(isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+
+    getName() {
+        return this.name;
+    }
+    getDescription() {
+        return this.description;
+    }
+    getDate() {
+        return this.date;
+    }
+    getIsCompleted() {
+        return this.isCompleted;
+    }
+}
