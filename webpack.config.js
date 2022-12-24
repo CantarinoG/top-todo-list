64f6a39270e6491ad53b6e6a1561c7d471c4884d
CantarinoG <guilhermecantarino1709@gmail.com>
2022-12-24 11:40:09 -0300

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'DoingIt',
         }),
-        new FaviconsWebpackPlugin('./src/favicon/logo.png')
+        new FaviconsWebpackPlugin('./src/favicon/logo.svg')
     ],
     output: {
         filename: 'main.js',
