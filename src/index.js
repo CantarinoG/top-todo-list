7569ffb083e6c8da32a2b4ab15348269459f7240
CantarinoG <guilhermecantarino1709@gmail.com>
2022-12-23 22:02:36 -0300

    Add task display design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/mainLayout.css';
 import './styles/header.css';
 import './styles/nav.css';
+import './styles/tasks.css';
 
 import { renderPage } from './logic/DOMManipulation';
 renderPage();
